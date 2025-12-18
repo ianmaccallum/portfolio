750f8cf62ec8502b626c973e8a27e31ca25c9ae3
Ian MacCallum <iancmaccallum@gmail.com>
2025-12-18 10:17:45 -0500

    hides max page size

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -3,6 +3,7 @@ import Image from 'next/image'
 
 import { Container } from '@/components/Container'
 import { FadeIn } from '@/components/FadeIn'
+import { LinkIcon, RocketIcon } from '@/components/icons'
 import { storeLinks } from '@/lib/info'
 import logoDiy from '@/images/projects/diy.png'
 import logoParra from '@/images/projects/parra.png'
@@ -11,17 +12,6 @@ import logoMojo from '@/images/projects/mojo.png'
 import downloadAppStore from '@/images/download-app-store.svg'
 import chromeIcon from '@/images/chrome.png'
 import safariIcon from '@/images/safari.png'
-
-function LinkIcon(props: React.ComponentPropsWithoutRef<'svg'>) {
-  return (
-    <svg viewBox="0 0 24 24" aria-hidden="true" {...props}>
-      <path
-        d="M15.712 11.823a.75.75 0 1 0 1.06 1.06l-1.06-1.06Zm-4.95 1.768a.75.75 0 0 0 1.06-1.06l-1.06 1.06Zm-2.475-1.414a.75.75 0 1 0-1.06-1.06l1.06 1.06Zm4.95-1.768a.75.75 0 1 0-1.06 1.06l1.06-1.06Zm3.359.53-.884.884 1.06 1.06.885-.883-1.061-1.06Zm-4.95-2.12 1.414-1.415L12 6.344l-1.415 1.413 1.061 1.061Zm0 3.535a2.5 2.5 0 0 1 0-3.536l-1.06-1.06a4 4 0 0 0 0 5.656l1.06-1.06Zm4.95-4.95a2.5 2.5 0 0 1 0 3.535L17.656 12a4 4 0 0 0 0-5.657l-1.06 1.06Zm1.06-1.06a4 4 0 0 0-5.656 0l1.06 1.06a2.5 2.5 0 0 1 3.536 0l1.06-1.06Zm-7.07 7.07.176.177 1.06-1.06-.176-.177-1.06 1.06Zm-3.183-.353.884-.884-1.06-1.06-.884.883 1.06 1.06Zm4.95 2.121-1.414 1.414 1.06 1.06 1.415-1.413-1.06-1.061Zm0-3.536a2.5 2.5 0 0 1 0 3.536l1.06 1.06a4 4 0 0 0 0-5.656l-1.06 1.06Zm-4.95 4.95a2.5 2.5 0 0 1 0-3.535L6.344 12a4 4 0 0 0 0 5.656l1.06-1.06Zm-1.06 1.06a4 4 0 0 0 5.657 0l-1.061-1.06a2.5 2.5 0 0 1-3.535 0l-1.061 1.06Zm7.07-7.07-.176-.177-1.06 1.06.176.178 1.06-1.061Z"
-        fill="currentColor"
-      />
-    </svg>
-  )
-}
 
 function ProjectLogo({ src }: { src: typeof logoDiy }) {
   return (
@@ -61,9 +51,13 @@ function StoreButton({
 export function Projects() {
   return (
     <Container className="mt-16 md:mt-20">
+      <h2 className="flex text-sm font-semibold text-zinc-900">
+        <RocketIcon className="h-6 w-6 flex-none" />
+        <span className="ml-3">Recent Projects</span>
+      </h2>
       <ul
         role="list"
-        className="mx-auto grid max-w-2xl grid-cols-1 gap-x-12 gap-y-16 sm:grid-cols-2 lg:max-w-5xl"
+        className="mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-12 gap-y-16 sm:grid-cols-2 lg:max-w-5xl"
       >
         {/* DIYProject.ai */}
         <FadeIn>
@@ -123,7 +117,7 @@ export function Projects() {
           </li>
         </FadeIn>
 
-        {/* Max Page Size */}
+        {/* Max Page Size - Hidden for now
         <FadeIn delay={200}>
           <li className="group relative flex flex-col items-start">
             <div className="relative z-10 flex items-center gap-3">
@@ -146,6 +140,7 @@ export function Projects() {
             </div>
           </li>
         </FadeIn>
+        */}
 
         {/* Mojo Score Keeper */}
         <FadeIn delay={300}>
